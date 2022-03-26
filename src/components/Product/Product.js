d40f870bbfd1bcd31d35a10fded5e6304689f390
Alamin-git <alamin2.mas@gmail.com>
2022-03-26 18:10:59 +0600

    design almost complit . font aws working

---
+++ src/components/Product/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Product.css'
+
+const Product = (props) => {
+    const { product } = props;
+    return (
+        <div className='single-product'>
+            <img className='product-image' src={product.img} alt="" />
+            <div className='product-details'>
+                <h3>{product.name}</h3>
+                <p><small>Price: </small>{product.price}</p>
+            </div>
+            <button className='cart-btn'>Add To Cart <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></button>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setProducts(data))
     }, [])
     return (
         <div className='products-container'>
-            <div className="product">
-                <img src="" alt="" />
-                <h3></h3>
-                <p><small>Price:</small>$</p>
-                <button>Add To Cart</button>
+            <div className="products">
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                    ></Product>)
+                }
             </div>
-            <div className="cart"></div>
+            <div className="cart">
+                <h2>laksdfhaklsdfh</h2>
+            </div>
         </div>
     );
 };
